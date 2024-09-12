0662e025ed6ace23fa4375d052881ba077d66ad0
kalmod <kalmod@live.com>
2024-09-11 20:54:19 -0400

    Initial setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+# must be used before pygame import
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+
+def main():
+    print("Starting asteroids!")
+
+
+if __name__ == "__main__":
+    main()
